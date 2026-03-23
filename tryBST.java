02ad6c912a144888fa51546b863226d78fba8fea
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-23 14:38:20 +0200

    Update tryBST.java

--- tryBST.java
+++ tryBST.java
@@ -1,7 +1,7 @@
 class tNode {
     int key;
     tNode left, right;
-    
+
     public tNode(int key) {
         this.key = key;
         left = right= null;
@@ -10,17 +10,39 @@ class tNode {
 public class tryBST {
     tNode root;
     public tNode insert(tNode node, int key) {
-        if(node==null)
+        if(node==null) {
             return new tNode(key);
-        
-        if(key< node.key)
-            node.left = insert(node.left, key);
-        else 
-            node.right = insert(node.right, key);
-        return node;
-        
+        }
 
+        if(key< node.key) {
+            node.left = insert(node.left, key);
+        } else {
+            node.right = insert(node.right, key);
+        }
+        return node;
 
     }
+    public void balancedBST(int start, int end){
+        if(start > end) {
+            return;
+        }
+        
+        int mid = (start + end)/ 2;
+        root = insert(root, mid);
+        
+        balancedBST(start, mid - 1);
+        balancedBST(mid + 1, end);
+    }
+    public boolean isBST(tNode node, int min, int max){
+        if(node == null){
+            return true;
+        }
+        if(node.key <= min || node.key >= max){
+            return false;
+        }
+        return isBST(node.left, min, node.key ) && isBST(node.right, node.key, max);
+        
+    }
     
+
 }
