da0dd79e0ca6e2e47494bccc9cc6d13d5360a3a9
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-23 14:51:28 +0200

    Added code to delete all the nodes with “names” that are even numbers.

--- tryBST.java
+++ tryBST.java
@@ -26,10 +26,10 @@ public class tryBST {
         if(start > end) {
             return;
         }
-        
+
         int mid = (start + end)/ 2;
         root = insert(root, mid);
-        
+
         balancedBST(start, mid - 1);
         balancedBST(mid + 1, end);
     }
@@ -41,8 +41,49 @@ public class tryBST {
             return false;
         }
         return isBST(node.left, min, node.key ) && isBST(node.right, node.key, max);
-        
+
     }
     
+    public  tNode findMin(tNode node){
+        while (node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+    //This is to delete the node
+    public tNode delete(tNode node, int key){
+        if (node == null){
+            return null;
+        }
+        
+        if(key < node.key){
+            node.left = delete(node.left,key);
+        } else if (key > node.key){
+            node.right = delete(node.right, key);
+        } else {
+            if(node.left == null && node.right == null){
+                return null;
+            }
+            if(node.left == null){
+                return node.right;
+            }
+            if(node.right == null){
+                return node.left;
+            }
+            
+            tNode temp = findMin(node.right);
+            node.key = temp.key;
+            node.right = delete(node.right, temp.key);
+        }
+        return node;
+    }
+    //Removing all even numbers
+    public void removeEven(int max){
+        for (int i = 2; i < max; i = i + 2){
+            root = delete(root, i);
+        }
+    }
+    
+
 
 }
