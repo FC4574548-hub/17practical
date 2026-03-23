465755ad430e8d42706fa9969de11d64db58cc21
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-23 14:24:37 +0200

    Update tryBST.java

--- tryBST.java
+++ tryBST.java
@@ -1,1 +1,26 @@
+class tNode {
+    int key;
+    tNode left, right;
+    
+    public tNode(int key) {
+        this.key = key;
+        left = right= null;
+    }
+}
+public class tryBST {
+    tNode root;
+    public tNode insert(tNode node, int key) {
+        if(node==null)
+            return new tNode(key);
+        
+        if(key< node.key)
+            node.left = insert(node.left, key);
+        else 
+            node.right = insert(node.right, key);
+        return node;
+        
 
+
+    }
+    
+}
